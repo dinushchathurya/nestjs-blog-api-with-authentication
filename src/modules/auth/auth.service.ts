22dd49e6f969e7ce7cd7362db2135762663f8730
dinushchathurya <dinushchathurya21@gmail.com>
2022-02-01 19:54:03 +0530

    feat: Finish basic authentication with JWT

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,23 +1,22 @@
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CommonErrors } from 'src/shared/errors/common-erros';
+import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
+
+import { CommonErrors } from 'src/shared/errors/common-erros';
 import { CreateUserDto } from './models/dto/create-user.dto';
+import { AuthLoginDto } from './models/dto/auth-login.dto';
 import { User } from './models/entities/user.entity';
 
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+    constructor(@InjectRepository(User) private userRepository: Repository<User>,  private jwtService: JwtService) { }
 
     async createUser(createuserDto: CreateUserDto) {
 
-        const existing = await this.userRepository.findOne({ 
-            where: { 
-                email: createuserDto.email 
-            } 
-        });
-        
+        const existing = await this.findByEmail(createuserDto.email);
+
         const user = await this.userRepository.create(createuserDto);
 
         if (existing) { 
@@ -33,5 +32,36 @@ export class AuthService {
         
         delete user.password;
         return user;
+    }
+
+    async login(authLoginDto: AuthLoginDto) {
+        const user = await this.validateUser(authLoginDto);
+
+        const payload = {
+            id: user.id,
+        };
+
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+
+    async validateUser(authLoginDto: AuthLoginDto): Promise<User> {
+        const { email, password } = authLoginDto;
+
+        const user = await this.findByEmail(email);
+        if (!(await user?.validatePassword(password))) {
+            throw new UnauthorizedException(CommonErrors.Unauthorized);
+        }
+
+        return user;
+    }
+
+    async findByEmail(email: string) {
+        return await User.findOne({
+            where: {
+                email: email,
+            },
+        });
     }
 }
