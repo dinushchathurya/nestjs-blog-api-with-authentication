188ada819ee0f2baf1b09684a10271fd34358b22
dinushchathurya <dinushchathurya21@gmail.com>
2022-02-01 19:09:36 +0530

    feat: Create password validation function

--- src/modules/auth/models/entities/user.entity.ts
+++ src/modules/auth/models/entities/user.entity.ts
@@ -24,4 +24,8 @@ export class User extends BaseEntity {
     async hashPassword() {
         this.password = await bcrypt.hash(this.password, 8);
     }
+
+    async validatePassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password);
+    }
 }
