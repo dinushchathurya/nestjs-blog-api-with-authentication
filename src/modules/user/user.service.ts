e41cbc01592ba40ca3cb25cfca232916ce6021d5
dinushchathurya <dinushchathurya21@gmail.com>
2022-02-05 20:28:16 +0530

    refactor: Remove unwanted imports

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CommonErrors } from 'src/shared/errors/common-erros';
-import { createQueryBuilder, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { User } from '../auth/models/entities/user.entity';
 
 @Injectable()
