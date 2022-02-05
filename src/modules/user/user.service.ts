0b771fa6e2801d2704f0d2aa67e283be17cc75a2
dinushchathurya <dinushchathurya21@gmail.com>
2022-02-05 15:11:59 +0530

    feat: Create function to get all users

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../auth/models/entities/user.entity';
+
+@Injectable()
+export class UserService {
+
+    constructor(@InjectRepository(User) private userRepository: Repository<User>,) { }
+    
+    async findAll(): Promise<User[]> {
+        return await this.userRepository.find();
+    }
+}
